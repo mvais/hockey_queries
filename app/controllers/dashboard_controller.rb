773fd91bd7ee9dcc9cd506dc7c24a5a2c1817e60
Vais Mahendran <vais.mahendran@gmail.com>
2021-02-28 21:39:55 -0500

    Add visitor, home records

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -41,12 +41,17 @@ class DashboardController < ApplicationController
         (vts.total_wins + hts.total_wins) total_wins,
         (vts.total_losses + hts.total_losses) total_losses,
         (vts.regulation_wins + hts.regulation_wins) regulation_wins,
+        (vts.regulation_wins + hts.regulation_wins + vts.overtime_wins + hts.overtime_wins) regulation_overtime_wins,
         (vts.overtime_wins + hts.overtime_wins) overtime_wins,
         (vts.shootout_wins + hts.shootout_wins) shootout_wins,
         (vts.regulation_losses + hts.regulation_losses) regulation_losses,
         (vts.overtime_losses + hts.overtime_losses) overtime_losses,
         (vts.shootout_losses + hts.shootout_losses) shootout_losses,
-        (vts.total_wins + hts.total_wins) * 2 + (vts.overtime_losses + hts.overtime_losses) points
+        (vts.total_wins + hts.total_wins) * 2 + (vts.overtime_losses + hts.overtime_losses + vts.shootout_losses + hts.shootout_losses) points,
+        CONCAT(hts.total_wins, '-', hts.regulation_losses, '-', hts.overtime_losses + hts.shootout_losses) AS home_record,
+        (hts.total_wins) * 2 + (hts.overtime_losses + hts.shootout_losses) home_record_points,
+        CONCAT(vts.total_wins, '-', vts.regulation_losses, '-', vts.overtime_losses + vts.shootout_losses) AS visitor_record,
+        (vts.total_wins) * 2 + (vts.overtime_losses + vts.shootout_losses) visitor_record_points
       FROM VISITOR_TEAM_STATS vts
       INNER JOIN HOME_TEAM_STATS hts ON vts.abrv = hts.abrv
       ORDER BY #{sort_column} #{sort_direction};
