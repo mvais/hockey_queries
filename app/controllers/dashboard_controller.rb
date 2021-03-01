696e3bb74b7d36153c67fe499ac7649933f919c1
Vais Mahendran <vais.mahendran@gmail.com>
2021-02-28 21:49:26 -0500

    Add goals_for, goals_against columns

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,7 +15,9 @@ class DashboardController < ApplicationController
             COUNT(CASE WHEN visitor_team_goals > home_team_goals AND game_ended_status = 'SO' THEN 0 END) shootout_wins,
             COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = '' THEN 0 END) regulation_losses,
             COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = 'OT' THEN 0 END) overtime_losses,
-            COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = 'SO' THEN 0 END) shootout_losses
+            COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = 'SO' THEN 0 END) shootout_losses,
+            SUM(visitor_team_goals) visitor_goals_for,
+            SUM(home_team_goals) visitor_goals_against
           FROM schedules
           GROUP BY visitor_team_abrv, visitor_team_name
         ), HOME_TEAM_STATS AS (
@@ -30,7 +32,9 @@ class DashboardController < ApplicationController
             COUNT(CASE WHEN visitor_team_goals > home_team_goals AND game_ended_status = 'SO' THEN 0 END) shootout_losses,
             COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = '' THEN 0 END) regulation_wins,
             COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = 'OT' THEN 0 END) overtime_wins,
-            COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = 'SO' THEN 0 END) shootout_wins
+            COUNT(CASE WHEN visitor_team_goals < home_team_goals AND game_ended_status = 'SO' THEN 0 END) shootout_wins,
+            SUM(home_team_goals) home_goals_for,
+            SUM(visitor_team_goals) home_goals_against
           FROM schedules
           GROUP BY home_team_abrv, home_team_name
         )
@@ -51,7 +55,9 @@ class DashboardController < ApplicationController
         CONCAT(hts.total_wins, '-', hts.regulation_losses, '-', hts.overtime_losses + hts.shootout_losses) AS home_record,
         (hts.total_wins) * 2 + (hts.overtime_losses + hts.shootout_losses) home_record_points,
         CONCAT(vts.total_wins, '-', vts.regulation_losses, '-', vts.overtime_losses + vts.shootout_losses) AS visitor_record,
-        (vts.total_wins) * 2 + (vts.overtime_losses + vts.shootout_losses) visitor_record_points
+        (vts.total_wins) * 2 + (vts.overtime_losses + vts.shootout_losses) visitor_record_points,
+        (hts.home_goals_for + vts.visitor_goals_for) goals_for,
+        (vts.visitor_goals_against + hts.home_goals_against) goals_against
       FROM VISITOR_TEAM_STATS vts
       INNER JOIN HOME_TEAM_STATS hts ON vts.abrv = hts.abrv
       ORDER BY #{sort_column} #{sort_direction};
